b894dec1cef5d1077e0136a6d961799ca6d76d11
William Bailly <222892737+WilliamBailly@users.noreply.github.com>
2025-09-09 16:52:12 -0700

    test apikey

--- playlist_analyzer.py
+++ playlist_analyzer.py
@@ -1,11 +1,15 @@
 import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 import pandas
 import matplotlib
+from dotenv import load_dotenv
+import os
 
 def main():
-    
-
-
+    load_dotenv()
+    client_id = os.getenv("client_id")
+    client_secret = os.getenv("client_secret")
+    print(client_id, client_secret)
 
 
 
