2848d19d44cb2139a7da2027131a7b09a0485f3a
William Bailly <222892737+WilliamBailly@users.noreply.github.com>
2025-09-10 09:13:02 -0700

    create get_playlist function

--- playlist_analyzer.py
+++ playlist_analyzer.py
@@ -2,14 +2,37 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import pandas
 import matplotlib
-from dotenv import load_dotenv
-import os
+import sys
+
+#Authenticate API Key
+auth_manager = SpotifyClientCredentials()
+sp = spotipy.Spotify(auth_manager=auth_manager)
 
 def main():
-    load_dotenv()
-    client_id = os.getenv("client_id")
-    client_secret = os.getenv("client_secret")
-    print(client_id, client_secret)
+    playlist = get_playlist()
+    for item in playlist:
+        track = item["track"]
+        print(track['name'])
+
+def get_playlist():
+    #Retreive playlistID
+    playlist_url = input("Enter your spotify playlist url: ")
+    parts = playlist_url.split('/')
+    playlist_id_with_params = parts[-1] 
+    if '?' in playlist_id_with_params:
+        playlist_id = playlist_id_with_params.split('?')[0]
+    else:
+        playlist_id = playlist_id_with_params
+    
+    #Retrieve playlist
+    results = sp.playlist_tracks(playlist_id)
+    tracks = results["items"]
+
+    while results["next"]:
+        results = sp.next(results)
+        tracks.extend(results["items"])
+    
+    return tracks
 
 
 
